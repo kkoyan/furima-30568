c8a7629f5ceceb4c37ae85eddd4c6a0aa93d6852
kkoyan <71924575+kkoyan@users.noreply.github.com>
2020-10-26 20:46:08 +0900

    ユーザー新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         with_options presence: true do
+          validates :nickname
+          validates :email
+          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :birthday
+        end
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+      end
